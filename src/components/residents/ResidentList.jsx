728999da4e192c5b133be1b8fdeb195cfb4d4b68
demetcaliskan <demetcaliskann@gmail.com>
2022-02-16 00:49:54 +0300

    unnecessary code block has been removed

--- src/components/residents/ResidentList.jsx
+++ src/components/residents/ResidentList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import RickAndMortyContext from '../../context/RickAndMortyContext'
 import ResidentItem from './ResidentItem'
@@ -10,21 +10,18 @@ function ResidentList() {
 
     const params = useParams()
 
-    const [readyForRender, setReadyForRender] = useState(false)
-
     useEffect(() => {
         getCharacters(params.id)
-        setReadyForRender(true)
     }, [])
 
 
-    if (readyForRender && !loading && characters.length <= 0) {
+    if (!loading && characters.length <= 0) {
         return (
             <div className='card shadow-xl shadow-violet-500/50 rounded-md bg-violet-900 p-5 m-8'>
                 <h2 className='font-mono font-bold text-xl text-white'>There is no resident living here!</h2>
             </div>
         )
-    } else if (!loading && readyForRender) {
+    } else if (!loading) {
         return (
             <div className='grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 p-5'>
                 {
