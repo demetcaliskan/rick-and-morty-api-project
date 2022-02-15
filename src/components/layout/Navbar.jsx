c3be5b2a395efc62adda372b39fbd2b637ea7ed8
demetcaliskan <demetcaliskann@gmail.com>
2022-02-16 01:33:57 +0300

    final touches has been done

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,14 +6,14 @@ function Navbar() {
         <nav className="bg-violet-800 font-mono" >
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <div className="flex-1 flex items-center justify-center sm:items-center sm:justify-start">
+                    <div className="flex-1 flex items-center justify-center md:items-center md:justify-start">
                         <div className="flex-shrink-0 flex items-center">
                             <img className="lg:block h-10 w-auto" src={logo} alt="RickAndMortyAPI" />
                             <h1 className="text-white font-bold px-3 py-2 rounded-md text-lg font-large">
                                 Rick And Morty Universe
                             </h1>
                         </div>
-                        <div className="hidden sm:block sm:ml-6 ">
+                        <div className="hidden md:block md:ml-6 ">
                             <div className="flex space-x-4">
                                 <Link to="/" className="text-center bg-gray-900 text-white block rounded-md p-2 px-7 font-medium shadow-xl  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">
                                     Locations
@@ -25,7 +25,7 @@ function Navbar() {
             </div>
 
 
-            <div className="sm:hidden font-mono" id="mobile-menu">
+            <div className="md:hidden font-mono" id="mobile-menu">
                 <div className="px-10 pt-2 pb-3 space-y-1">
                     <Link to="/" className="text-center bg-gray-900 text-white block rounded-md p-2 px-7 font-medium shadow-xl  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">
                         Locations
--- src/components/locations/LocationItem.jsx
+++ src/components/locations/LocationItem.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 function LocationItem({ location: { id, type, dimension, residents } }) {
 
     return (
-        <div className='min-w-full card shadow-xl rounded-md bg-violet-300 p-5 xl:flex xl:justify-between lg:flex lg:justify-between md:grid md:justify-items-center sm:grid sm:justify-items-center sm:place-items-center shadow-indigo-600/50'>
-            <div className='flex flex-row'>
+        <div className='min-w-full card shadow-xl rounded-md bg-violet-300 p-5 xl:flex xl:justify-between lg:flex lg:justify-between sm:grid sm:justify-items-center sm:gap-2 shadow-indigo-600/50'>
+            <div className='flex flex-row justify-center'>
                 <div>
                     <FcNightLandscape className='h-20 w-20 pr-10' />
                 </div>
                 <div>
-                    <div className='flex flex-row gap-4'>
+                    <div className='flex xl:flex-row lg:flex-row md:flex-row sm:flex flex-col xl:gap-4 lg:gap-4 md:gap-4'>
                         <h3 className='font-extrabold'>Location Type:</h3>
                         <h3 className='font-black italic text-teal-700 text-lg'>{type}</h3>
                     </div>
@@ -21,7 +21,7 @@ function LocationItem({ location: { id, type, dimension, residents } }) {
                 </div>
             </div>
 
-            <div className='py-3'>
+            <div className='py-3 flex flex-row justify-center'>
                 <Link
                     to={`location/${id}/residents`}
                     className='font-mono p-3 rounded-md text-lg text-teal-500 shadow-xl shadow-green-600/40 font-small transition duration-500 ease-in-out bg-gray-900 hover:bg-violet-900 transform hover:-translate-y-1 hover:scale-110'
